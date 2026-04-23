1ffadcd5a76bade95ce19c3591c4c81a54ea43ca
Asif <asif@example.com>
2026-04-23 09:59:25 +0530

    fix app

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const express = require("express");
+kconst express = require("express");
 const app = express();
 
-app.use(express.json());
-
-// Health check
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "UP" });
-});
-
-// Sample users API
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "Alice" },
-    { id: 2, name: "Bob" }
-  ];
-  res.json(users);
-});
-
-// Start server
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Azure 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
